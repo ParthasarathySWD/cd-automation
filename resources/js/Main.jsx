2c1820b9b5f306eab01d2467c897e19c2ca541b3
mohindarkumar-v <mohindar.kumar@avanzegroup.com>
2021-02-12 14:20:01 +0530

    Remove Layout Files

---
+++ resources/js/Main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import Header from './layouts/Header';
+import LeftSideBar from './layouts/LeftSideBar';
+import RightSideBar from './layouts/RightSideBar';
+import Footer from './layouts/Footer';
+
+class Main extends React.Component {
+   render() {
+      return (
+        <div className="theme-cyan">
+            
+            <div className="themesetting"></div>
+            
+            
+            <div className="overlay"></div>
+            
+            <div id="wrapper">
+              
+              
+              <nav className="navbar navbar-fixed-top">
+                <Header/>
+              </nav>
+                
+                
+              <div id="left-sidebar" className="sidebar">
+                <LeftSideBar/>
+              </div>
+                
+                
+              <div id="rightbar" className="rightbar">
+                <RightSideBar/>
+              </div>
+                
+                
+              <div className="sticky-note"></div>
+
+                
+              <div id="main-content">
+                <div className="container-fluid">
+                  <div className="block-header"></div>
+                  <div className="row clearfix"></div>
+                </div>
+              </div>
+                
+            </div>
+        </div>
+        );
+    }
+}
+export default Main;
