bd6ce7f489df4396e9a19d6666beb3f28c663ca3
mohindarkumar-v <mohindar.kumar@avanzegroup.com>
2021-02-13 20:10:09 +0530

    Theme Changes

--- resources/js/components/UsersComponents/AllUsers.jsx
+++ resources/js/components/UsersComponents/AllUsers.jsx
@@ -24,8 +24,36 @@ class AllUsers extends React.Component{
                     </div>
 				</div>
 				<div className="row clearfix">
-					<div>
-						<h5>Tables</h5>
+					<div className="col-md-12">
+						<table className="table">
+						  <thead>
+						    <tr>
+						      <th scope="col">#</th>
+						      <th scope="col">First Name</th>
+						      <th scope="col">Last Name</th>
+						      <th scope="col">Email</th>
+						    </tr>
+						  </thead>
+						  <tbody>
+						    <tr>
+						      <th scope="row">1</th>
+						      <td>Mark</td>
+						      <td>Otto</td>
+						      <td>@mdo</td>
+						    </tr>
+						    <tr>
+						      <th scope="row">2</th>
+						      <td>Jacob</td>
+						      <td>Thornton</td>
+						      <td>@fat</td>
+						    </tr>
+						    <tr>
+						      <th scope="row">3</th>
+						      <td colSpan="2">Larry the Bird</td>
+						      <td>@twitter</td>
+						    </tr>
+						  </tbody>
+						</table>
 					</div>
 				</div>
 			</div>
