cdcca9af2cc6e4b055c94bde0f3852c01f6bfa13
MuthuRajam2210 <muthurajam.s@avanzegroup.com>
2021-02-23 20:09:03 +0530

    MyOrder data table

--- resources/js/components/LoginComponents/ChangePassword.jsx
+++ resources/js/components/LoginComponents/ChangePassword.jsx
@@ -4,24 +4,33 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 class ForgotPassword extends React.Component{
     render() {
         return(
-            <div className="ForgetPassword-container col-sm-6 card">
+            <div className="ChangePassword-container col-sm-6 card">
                 <div id="card text-center"  >
                         <div className="head">
-                            <p className="leads">Forgot Password</p>
+                            <p className="change-leads">Change Password</p>
                             <hr></hr>
                         </div>
                         <div className="body">
-                            <form className="form-auth-small" action="index.html">
+                            <form className="form-auth-small">
                                
                                 <div className="form-group ">
-                                    <label className="field-label">Email Id</label>
-                                    <input type="email" className="form-control" autoFocus="autofocus"/>
+                                    <label className="field-label">Old Password</label>
+                                    <input type="password" className="form-control" autoFocus="autofocus"/>
+                                </div>
+
+                                <div className="form-group ">
+                                    <label className="field-label">New Password</label>
+                                    <input type="password" className="form-control"/>
+                                </div>
+
+                                <div className="form-group ">
+                                    <label className="field-label">Confirm Password</label>
+                                    <input type="password" className="form-control"/>
                                 </div>
                                 
                                 <button type="submit" id="signup-btn" className="btn btn-lg btn-block"><b>Submit</b></button>
                                  <div className="bottom text-center"> 
                                     <span className="helper-text m-b-10 text-center text-secondary"><i className="fa fa-arrow-left text-primary text-secondary"></i> <Link to={'/'} className="text-secondary">Back to login</Link></span>
-                                    {/* <span>Don't have an account? <a href="page-register.html">Register</a></span> */}
                                  </div> 
                             </form>
                         </div>
--- resources/js/components/MyOrderPageComponents/MyOrder.jsx
+++ resources/js/components/MyOrderPageComponents/MyOrder.jsx
@@ -1,6 +1,86 @@
 import React from 'react';
 import '../../../css/MyOrder/MyOrder.css';
+import DataTable from 'react-data-table-component';
+import DataTab from './DataTab';
 
+const columns = [
+    {
+      name: <b>Order Id</b>,
+      selector: "OrderId",
+      sortable: true
+    },
+    {
+      name: <b>Loan Number</b>,
+      selector: "LoanNumber",
+      sortable: true
+    },
+    
+      {
+        name: <b>Customer</b>,
+        selector: "Customer",
+        sortable: true
+      },
+      {
+        name: <b>Loan Type</b>,
+        selector: "LoanType",
+        sortable: true
+      },
+      {
+        name: <b>Status</b>,
+        selector: "status",
+        sortable: true
+        
+      },
+      {
+          name:<b>Action</b>,
+          selector:"action",
+          cell: row => <div><a href="#"><span className="fa fa-eye text-primary p-1"></span></a>
+          <a href="#"><span className="fa fa-edit text-secondary p-1"></span></a>
+          <a href="#"><span className="fa fa-trash text-danger p-1"></span></a>
+          </div>
+    //       render: (rowData) =>
+    //       rowData && (
+    //     //   <IconButton
+    //     //       color="secondary">
+    //           <a href="#"><span className="fa fa-trash"></span></a>
+    //     //   </IconButton>
+    //       )
+      }
+     
+  ];
+
+  const Checkbox = React.forwardRef(({ onClick, ...rest }, ref) => (
+    <div className="custom-control custom-checkbox">
+      <input
+        type="checkbox"
+        className="custom-control-input"
+        ref={ref}
+        {...rest}
+      />
+     
+      <label className="custom-control-label" onClick={onClick} />
+    
+    </div>
+  
+  ));
+
+//   const action = React.forwardRef(({ onClick, ...rest }, ref) => (
+//     <div className="action">
+//       <a href="#"><span className="fa fa-trash"
+//         ref={ref}
+//         {...rest}>
+//     </span></a>
+//     <a href="#"><span className="fa fa-eye"
+//         ref={ref}
+//         {...rest}>
+//     </span></a>
+     
+//       <label className="action-label" onClick={onClick} />
+    
+//     </div>
+   
+//   ));
+  
 class MyOrder extends React.Component{
     render(){
         return(
@@ -11,21 +91,21 @@ class MyOrder extends React.Component{
                     </div> 
                     <div className="child-container">
                         <div className="row mb-2 status-row">
-                            <div className="col-xl-3 col-sm-6 py-2">
+                            <div className="col-sm-3 col-md-6 col-lg-4 py-2 status-container">
                                 <div className="card bg-light h-100 status-val">
                                     <label>Active Orders</label>
                                     <h4>1,023</h4>
                                 </div>
                             </div>
 
-                            <div className="col-xl-3 col-sm-6 py-2">
+                            <div className="col-sm-3 col-md-6 col-lg-4 py-2 status-container">
                                 <div className="card bg-light h-100 status-val">
                                     <label>Pending</label>
                                     <h4>64</h4>
                                 </div>
                             </div>
                     
-                            <div className="col-xl-3 col-sm-6 py-2">
+                            <div className="col-sm-3 col-md-6 col-lg-4 py-2 status-container">
                                 <div className="card bg-light h-100 status-val">
                                     <label>Completed</label>
                                     <h4>990</h4>
@@ -35,98 +115,51 @@ class MyOrder extends React.Component{
                         <div className="tab-menus">
                             <ul className="nav nav-pills nav-menu" role="tablist">
                                 <li className="nav-item">
-                                <a className="nav-link" href="#all" role="tab" data-toggle="tab">Active orders</a>
+                                <a className="nav-link" href="#all" role="tab" data-toggle="pill">Active orders</a>
                                 </li>
                                 <li className="nav-item">
-                                <a className="nav-link" href="#unpaid" role="tab" data-toggle="tab">Pending</a>
+                                <a className="nav-link" href="#pending" role="tab" data-toggle="pill">Pending</a>
                                 </li>
                                 <li className="nav-item">
-                                <a className="nav-link" href="#unfulfilled" role="tab" data-toggle="tab">Completed</a>
+                                <a className="nav-link" href="#completed" role="tab" data-toggle="pill">Completed</a>
                                 </li>
                             </ul>
                         </div>
 
-                        <div className="order-table">
-                            <table className="table table-striped" id="all">
-                                <thead>
-                                    <tr>
-                                        <th><input type="checkbox"/></th>
-                                        <th>Order Id</th>
-                                        <th>Loan Number</th>
-                                        <th>Customer</th>
-                                        <th>Loan Type</th>
-                                        <th>Status</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td><input type="checkbox"></input></td>
-                                        <td>121 091</td>
-                                        <td>102223234</td>
-                                        <td><img src="../../images/robot.png" className="rounded dp" width="20px"></img><label>Amy</label></td>
-                                        <td>Housing Loan</td>
-                                        <td><span className="badge badge-secondary">In progress</span></td>
-                                        <td>
-                                            <a href="#"  className="fa fa-eye icons text-primary"></a> 
-                                            <a href="#"  className="fa fa-edit icons text-success"></a>  
-                                            <a href="#"  className="fa fa-trash icons text-danger"></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox"></input></td>
-                                        <td>121 091</td>
-                                        <td>102223234</td>
-                                        <td><img src="../../images/robot.png" className="rounded dp" width="20px"></img><label>Richard</label></td>
-                                        <td>Personal Loan</td>
-                                        <td><span className="badge badge-success p-1">Closed</span></td>
-                                        <td>
-                                            <a href="#"  className="fa fa-eye icons text-primary"></a> 
-                                            <a href="#"  className="fa fa-edit icons text-success"></a>  
-                                            <a href="#"  className="fa fa-trash icons text-danger"></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox"></input></td>
-                                        <td>121 091</td>
-                                        <td>102223234</td>
-                                        <td><img src="../../images/robot.png" className="rounded dp" width="20px"></img><label>Sara Graham</label></td>
-                                        <td>Education Loan</td>
-                                        <td><span className="badge badge-secondary p-1">In progress</span></td>
-                                        <td>
-                                            <a href="#"  className="fa fa-eye icons text-primary"></a> 
-                                            <a href="#"  className="fa fa-edit icons text-success"></a>  
-                                            <a href="#"  className="fa fa-trash icons text-danger"></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox"></input></td>
-                                        <td>121 081</td>
-                                        <td>1024444234</td>
-                                        <td><img src="../../images/robot.png" className="rounded dp" width="20px"></img><label>Victor Samson</label></td>
-                                        <td>Personal Loan</td>
-                                        <td><span className="badge badge-primary p-1">Open</span></td>
-                                        <td>
-                                            <a href="#"  className="fa fa-eye icons text-primary"></a> 
-                                            <a href="#"  className="fa fa-edit icons text-success"></a>  
-                                            <a href="#"  className="fa fa-trash icons text-danger"></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="checkbox"></input></td>
-                                        <td>121 081</td>
-                                        <td>1034323234</td>
-                                        <td><img src="../../images/robot.png" className="rounded dp" width="20px"></img><label>Patrica John</label></td>
-                                        <td>Education Loan</td>
-                                        <td><span className="badge badge-success p-1">Closed</span></td>
-                                        <td>
-                                            <a href="#"  className="fa fa-eye icons text-primary"></a> 
-                                            <a href="#"  className="fa fa-edit icons text-success"></a>  
-                                            <a href="#"  className="fa fa-trash icons text-danger"></a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                        <div className="tab-content">
+                            <div id="all" className="order-table tab-pane in active">
+                                <DataTable
+                                    columns={columns}
+                                    data={DataTab}
+                                    defaultSortField="title"
+                                    pagination
+                                    selectableRows
+                                    selectableRowsComponent={Checkbox}
+                                    // selectableRowsComponent={action}
+                                    />
+                            </div>
+                            <div id="pending" className="order-table tab-pane fade">
+                                <DataTable
+                                    columns={columns}
+                                    data={DataTab}
+                                    defaultSortField="title"
+                                    pagination
+                                    selectableRows
+                                    selectableRowsComponent={Checkbox}
+                                    // selectableRowsComponent={action}
+                                    />
+                            </div>
+                            <div id="completed" className="order-table tab-pane fade">
+                                <DataTable
+                                    columns={columns}
+                                    data={DataTab}
+                                    defaultSortField="title"
+                                    pagination
+                                    selectableRows
+                                    selectableRowsComponent={Checkbox}
+                                    // selectableRowsComponent={action}
+                                    />
+                            </div>
                         </div>
                     </div>
             </div>
