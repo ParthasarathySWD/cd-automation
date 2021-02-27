1e4eecd96f03b69c2c1fef5f40fa3341e0bce49d
MuthuRajam2210 <muthurajam.s@avanzegroup.com>
2021-02-27 12:23:21 +0530

    Myorder page search box UI

--- resources/js/components/MyOrderPageComponents/MyOrder.jsx
+++ resources/js/components/MyOrderPageComponents/MyOrder.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../css/MyOrder/MyOrder.css';
 import DataTable from 'react-data-table-component';
 import DataTableExtensions from "react-data-table-component-extensions";
 import "react-data-table-component-extensions/dist/index.css";
 import { BrowserRouter as Router, Switch, Route, Link, useHistory } from 'react-router-dom';
-import MyOrders from '../../components/Datatablecomponents/MyOrders';
-
 // import { columns, data } from './DataTab';
 import axios from 'axios';
 
@@ -17,6 +15,83 @@ const customStyles = {
     }
   }
 }
+const columns = [
+  {
+    name: <b>Order Number</b>,
+    selector: "OrderId",
+    sortable: true
+    
+  },
+  {
+    name: <b>Loan Number</b>,
+    selector: "LoanNumber",
+    sortable: true
+  },   
+  {
+    name: <b>Customer</b>,
+    selector: "Customer",
+    sortable: true
+
+  },
+  {
+    name: <b>Loan Type</b>,
+    selector: "LoanType",
+    sortable: true
+  },
+  {
+    name: <b>Status</b>,
+    selector: "status",
+    sortable: true
+      
+  },
+  {
+    name:<b>Action</b>,
+    selector:"action",
+    cell: row => <div><a href="#"><span className="fa fa-eye text-primary p-1"></span></a>
+    <a href="#"><span className="fa fa-edit text-secondary p-1"></span></a>
+    <a href="#"><span className="fa fa-trash text-danger p-1"></span></a>
+    </div>
+  }
+   
+];
+
+const data=[
+  {
+      OrderId: "100 291",
+      LoanNumber: "1024444234",
+      Customer:"Amy",
+      LoanType:"Housing",
+      status:"Pending"
+  },
+  {
+      OrderId: "100 231",
+      LoanNumber: "1223343455",
+      Customer:"Sarah Graham",
+      LoanType:"Education",
+      status:"Completed"    
+  }
+];
+
+/*Set checkbox for datatable rows*/
+// const Checkbox = React.forwardRef(({ onClick, ...rest }, ref) => (
+//   <div className="custom-control custom-checkbox">
+//     <input
+//       type="checkbox"
+//       className="custom-control-input"
+//       ref={ref}
+//       {...rest}
+//     />
+   
+//     <label className="custom-control-label" onClick={onClick} />
+  
+//   </div>
+
+// ));
+
+const tableData = {
+  columns,
+  data
+};
 
 
 
@@ -24,144 +99,9 @@ const customStyles = {
 
   const [users, setUsers] = useState({});
   const [page, setPage] = useState(1);
-  const [countPerPage, setCountPerPage] = useState(10);
-  
-
+  const countPerPage = 3;
 
   
-  /* Should be passed from props starts */
-  const fetchUsers = async (page, size = countPerPage, searchText = "") => {
-    
-    const response = await axios.post(
-      `myorders/fetchorders`,
-      {
-        rowCount: size,
-        page: page,
-        searchText: searchText,
-      }
-      );
-      console.log(response);
-      return response;
-    };
-    
-    const handleDelete = async (row) => {
-        await axios.delete(`https://reqres.in/api/users/${row.id}`);
-    }
-    
-    const columndata = [
-      {
-        name: "Order Number",
-        selector: "OrderNumber",
-        sortable: true
-      },
-      {
-        name: "Loan Number",
-        selector: "LoanNumber",
-        sortable: true
-      },
-      {
-        name: "Client",
-        selector: "ClientUID",
-        sortable: true
-      },
-      {
-        name: "Status",
-        selector: "StatusUID",
-        sortable: true
-      },
-      {
-        // eslint-disable-next-line react/button-has-type
-        name:<b>Action</b>,
-        selector:"action",
-        cell: row => <div><a href="#"><span className="fa fa-eye text-primary p-1"></span></a>
-        <a href="#"><span className="fa fa-edit text-secondary p-1"></span></a>
-        <a href="#"><span className="fa fa-trash text-danger p-1"></span></a>
-        </div>
-      }
-    ];
-    /* Should be passed from props Ends */
-
-
-
-
-  const columns = [
-    {
-      name: <b>Order Number</b>,
-      selector: "OrderId",
-      sortable: true
-      
-    },
-    {
-      name: <b>Loan Number</b>,
-      selector: "LoanNumber",
-      sortable: true
-    },   
-    {
-      name: <b>Customer</b>,
-      selector: "Customer",
-      sortable: true
-
-    },
-    {
-      name: <b>Loan Type</b>,
-      selector: "LoanType",
-      sortable: true
-    },
-    {
-      name: <b>Status</b>,
-      selector: "status",
-      sortable: true
-        
-    },
-    {
-      name:<b>Action</b>,
-      selector:"action",
-      cell: row => <div><a href="#"><span className="fa fa-eye text-primary p-1"></span></a>
-      <a href="#"><span className="fa fa-edit text-secondary p-1"></span></a>
-      <a href="#"><span className="fa fa-trash text-danger p-1"></span></a>
-      </div>
-    }
-     
-  ];
-
-  const data=[
-    {
-        OrderId: "100 291",
-        LoanNumber: "1024444234",
-        Customer:"Amy",
-        LoanType:"Housing",
-        status:"Pending"
-    },
-    {
-        OrderId: "100 231",
-        LoanNumber: "1223343455",
-        Customer:"Sarah Graham",
-        LoanType:"Education",
-        status:"Completed"    
-    }
-  ];
-  
-  /*Set checkbox for datatable rows*/
-  // const Checkbox = React.forwardRef(({ onClick, ...rest }, ref) => (
-  //   <div className="custom-control custom-checkbox">
-  //     <input
-  //       type="checkbox"
-  //       className="custom-control-input"
-  //       ref={ref}
-  //       {...rest}
-  //     />
-     
-  //     <label className="custom-control-label" onClick={onClick} />
-    
-  //   </div>
-  
-  // ));
-  
-  const tableData = {
-    columns,
-    data
-  };
-
   
         return(
             <div className="main-container">
@@ -171,13 +111,13 @@ const customStyles = {
                   </div> 
                   <div className="child-container">
                         <div className="row mb-2 status-row">
-                          <div className="col-lg-4 col-md-4 col-sm-6 col-12 mb-2 mt-4">
-                              <div className="info">
-                                <div className="row">
-                                  <div className="col-lg-3 col-md-4 col-sm-4 col-4 rone">
+                          <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-2 mt-4">
+                              <div class="info">
+                                <div class="row">
+                                  <div class="col-lg-3 col-md-4 col-sm-4 col-4 rone">
                                     <i className="fa fa-file-text fa-2x"></i>
                                   </div>
-                                  <div className="col-lg-9 col-md-8 col-sm-8 col-8 fontsty">
+                                  <div class="col-lg-9 col-md-8 col-sm-8 col-8 fontsty">
                                       <label className="status-text">All Orders</label>
                                       <h4>2,210</h4>
                                   </div>
@@ -185,13 +125,13 @@ const customStyles = {
                               </div>
                           </div>
 
-                          <div className="col-lg-4 col-md-4 col-sm-6 col-12 mb-2 mt-4">
-                              <div className="info">
-                                <div className="row">
-                                  <div className="col-lg-3 col-md-4 col-sm-4 col-4 rone rtwo">
+                          <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-2 mt-4">
+                              <div class="info">
+                                <div class="row">
+                                  <div class="col-lg-3 col-md-4 col-sm-4 col-4 rone rtwo">
                                     <i className="fa fa-address-card fa-2x"></i>
                                   </div>
-                                  <div className="col-lg-9 col-md-8 col-sm-8 col-8 fontsty">
+                                  <div class="col-lg-9 col-md-8 col-sm-8 col-8 fontsty">
                                       <label className="status-text">Pending</label>
                                       <h4>107</h4>
                                   </div>
@@ -199,13 +139,13 @@ const customStyles = {
                               </div>
                           </div>
                     
-                          <div className="col-lg-4 col-md-4 col-sm-6 col-12 mb-2 mt-4">
-                              <div className="info">
-                                <div className="row">
-                                  <div className="col-lg-3 col-md-4 col-sm-4 col-4 rone rthree">
+                          <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-2 mt-4">
+                              <div class="info">
+                                <div class="row">
+                                  <div class="col-lg-3 col-md-4 col-sm-4 col-4 rone rthree">
                                     <i className="fa fa-handshake-o fa-2x"></i>
                                   </div>
-                                  <div className="col-lg-9 col-md-8 col-sm-8 col-8 fontsty">
+                                  <div class="col-lg-9 col-md-8 col-sm-8 col-8 fontsty">
                                       <label className="status-text">Completed</label>
                                       <h4>68</h4>
                                   </div>
@@ -228,26 +168,49 @@ const customStyles = {
                                 <a className="nav-link" href="#completed" role="tab" data-toggle="pill">Completed</a>
                                 </li>
                               </ul>
-                            {/* <input type="text" className="search-input" placeholder="search"></input> */}
-                            
+                            <input type="text" className="search-input" placeholder="&#61442; search"></input>
+                           
                           </div>
                            
                         </div>
-                      <div className="tab-values">
+                      <div className="tab-values p-10">
                         <div className="tab-content">
                             <div id="all" className="order-table tab-pane in active">
                               {/* <h4>Active Order List</h4> */}
-                              <DataTableExtensions {...tableData} filterPlaceholder={'Search'} export={false} print={false}> 
-                                  <MyOrders                                                                  
-                                  title = ""
-                                  columndata = {columndata}
-                                  fetchData = {fetchUsers}
-                                  setPerPage = {setCountPerPage}
-                                  />
-                              </DataTableExtensions>
+                              {/* <DataTableExtensions {...tableData} filterPlaceholder={'Search'} export={false} print={false}>  */}
+                                 
+                                <DataTable
+                                  columns={columns}
+                                  // data={users.data}
+                                  data={data}
+                                  defaultSortField="title"
+                                  pagination
+                                  customStyles={customStyles}
+                                  // selectableRows
+                                  // selectableRowsComponent={Checkbox}
+                    
+                                  // highlightOnHover
+                                  // pagination
+                                  // paginationServer
+                                  // paginationTotalRows={users.total}
+                                  // paginationPerPage={countPerPage}
+                                  // paginationComponentOptions={{
+                                  //   noRowsPerPage: true
+                                  // }}
+                                  // onChangePage={page => setPage(page)}
+                                />
+                              {/* </DataTableExtensions> */}
+                              {/* <DataTable
+                                      columns={columns}
+                                      data={DataTab}
+                                      defaultSortField="title"
+                                      pagination
+                                      selectableRows
+                                      selectableRowsComponent={Checkbox}
+                                      /> */}
                             </div>
                             <div id="pending" className="order-table tab-pane fade">
-                              <DataTableExtensions {...tableData} filterPlaceholder={'Search'} export={false}  print={false} >
+                              {/* <DataTableExtensions {...tableData} filterPlaceholder={'Search'} export={false}  print={false} > */}
                                 <DataTable
                                     columns={columns}
                                     data={data}
@@ -257,10 +220,10 @@ const customStyles = {
                                     // selectableRows
                                     // selectableRowsComponent={Checkbox}
                                     />
-                              </DataTableExtensions>
+                              {/* </DataTableExtensions> */}
                             </div>
                             <div id="completed" className="order-table tab-pane fade">
-                              <DataTableExtensions {...tableData} filterPlaceholder={'Search'} export={false} print={false} >
+                              {/* <DataTableExtensions {...tableData} filterPlaceholder={'Search'} export={false} print={false} > */}
                                 <DataTable
                                     columns={columns}
                                     data={data}
@@ -270,7 +233,7 @@ const customStyles = {
                                     // selectableRows
                                     // selectableRowsComponent={Checkbox}
                                     />
-                              </DataTableExtensions>
+                              {/* </DataTableExtensions> */}
                             </div>
                         </div>
                     </div>
