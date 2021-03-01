0c49c002561d52f5078b9325c022505428da30d6
Parthasarathy <parthaintodesk@gmail.com>
2021-03-01 19:16:15 +0530

    Auth disabled

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ use Illuminate\Support\Facades\Route;
 // 
 Route::view('/{path?}', 'welcome');
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
