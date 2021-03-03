0a8b4161a97c05d7e63728b5cb1663b6b7b28a93
mohindarkumar-v <mohindar.kumar@avanzegroup.com>
2021-03-03 12:13:48 +0530

    mUsers Alert Query

---
+++ database/migrations/2021_03_03_062040_create_musers_alter_query.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMusersAlterQuery extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mUsers', function (Blueprint $table) {
+            if (!Schema::hasColumn('mUsers', 'Active')) {
+                $table->integer('Active');
+            }
+        });
+
+        Schema::table('mRoles', function (Blueprint $table) {
+            if (!Schema::hasColumn('mRoles', 'Active')) {
+                $table->integer('Active');
+            }
+        });        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       //
+    }
+}
