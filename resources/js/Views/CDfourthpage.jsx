017d462fa664d52628b6713bf58ad258a49cc5b6
shoban0102 <shoban.baarathi@avanzegroup.com>
2021-03-03 20:52:13 +0530

    CDfourthpage

--- resources/js/Views/CDfourthpage.jsx
+++ resources/js/Views/CDfourthpage.jsx
@@ -39,49 +39,39 @@ function CDfourthpage(props) {
                                 If you sell or transfer this property to an other person, your lender <br/>
                                 <input type="checkbox"/>  will allow, under certain conditions, this person to assume this<br/>
                                 &nbsp; loan on the original terms,<br/>
-                                <input type="checkbox"/> will not allow assumption of this loan on the original terms,
+                                <input type="checkbox"/> will not allow assumption of this loan on the original terms.
                                 </p>
                                 <p>
                                 <b>Demand feature</b><br/>
                                 Your loan<br/>
-                                <input type="checkbox"/> has a demand feature, which permits your lender to require early<br/>
-                                &nbsp; repayment of the loan, You should review your note for details.<br/>
+                                <input type="checkbox"/> has a demand feature, which permits your lender to require early repayment of the loan, You should review your note for details. <br/>  
                                 <input type="checkbox"/> does not have a demand feature. 
                                 </p>
                                 <p>
                                 <b>Late Payment</b><br/>
-                                If your payment is more than 15 days late, your lender will charge a<br/>
-                                late fee of 5% the monthly prindpal and interest payment
+                                If your payment is more than rs days late, your lender will charge a late fee of 5% cl the monthly principal and interest payment. 
                                 </p>
                                 <p>
-                                <b>Negative Amortisation</b> (Increase in l oan Amount) <br/>
+                                <b>Negative Amortisation</b> (Increase in loan Amount) <br/>
                                 Under your loan terms, you <br/>
-                                <input type="checkbox"/> are scheduled to make monthly payments that do not pay all of <br/>
-                                &nbsp; the interest due that month. As a result, yourloa n amount will <br/>
-                                &nbsp; increase (negatively amortize), and your loan amount will likely <br/>
-                                &nbsp; bee om e larger than your origi na I loan amount. Increases in your <br/>
-                                &nbsp; I oan amount lower the equity you have in this prope tty, <br/>
-                                <input type="checkbox"/> mitf have monthly payments that do not pay all of the interest <br/>
-                                &nbsp; due that month. If you do, your loan amount will increase <br/>
-                                &nbsp; (negatively amortize1 and, as a result, your loan amount mitf <br/>
-                                &nbsp; bee om e larger than your origi na I loan amount. Increases in your <br/>
-                                &nbsp; I oan amount lower the equity you have in this prope tty, <br/>
-                                <input type="checkbox"/> do not have a negative amortization feature. 
+                                <input type="checkbox"/> are scheduled to make monthly payments that do not pay all of the interest due that month.
+                                 As a result, your loan amount will increase (negatively amortize), and your loan amount will likely become larger than your original loan amount.
+                                 Increases in your Ioan amount lower the equity you have in this property. <br/>
+                                <input type="checkbox"/> may have monthly payments that do not pay all of the interest due that month.
+                                 If you do, your loan amount will increase(negatively amortize) and, as a result, your loan amount may become larger than your original loan amount.
+                                 Increases in your Ioan amount lower the equity you have in this property. <br/>
+                                <input type="checkbox"/> do not have a negative amortization feature.  
                                 </p>
                                 <p>
                                 <b>Partial Payments</b><br/>
                                 Your lender<br/>
-                                <input type="checkbox"/> mitf accept pitfments that are less than the full amount due<br/>
-                                &nbsp; (partial payments) and apply them to your loan,<br/>
-                                <input type="checkbox"/> mitf hold them in a separate account until you pay the rest of the<br/>
-                                &nbsp; payment, and then apply the full payment to your loan,<br/>
-                                <input type="checkbox"/> does not accept any partial pitfments,<br/>
-                                If this loan is sold, your new lend er mil/ have a different pol icy,<br/>
+                                <input type="checkbox"/> may accept payments that are less than the full amount due (partial payments) and apply them to your loan. <br/> 
+                                <input type="checkbox"/> may hold them in a separate account until you pay the rest of the payment, and then apply the full payment to your loan. <br/> 
+                                <input type="checkbox"/>  does not accept any partial payments. <br/> If this loan is sold, your new lender may/ have a different policy. 
                                 </p>
                                 <p>
                                 <b>Security Interest</b><br/>
-                                You are granting a security interest in<br/>
-                                4 56 Somewhere Ave., Anytown, ST J ll4S <br/>
+                                You are granting a security interest in 456 Somewhere Ave., Anytown, ST 12345
                                 <br/>
                                 <br/>
                                 <br/>
@@ -92,11 +82,7 @@ function CDfourthpage(props) {
                                 <br/>
                                 <br/>
                                 <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                You mitf lose this property if you do not makeywr payments or <br/>
-                                satisfy other obi igat ions for this loan. 
+                                You may lose this property if you do not make your payments or satisfy other obligations for this loan.  
                                 </p>
                             </div>
                         </div>
@@ -104,12 +90,10 @@ function CDfourthpage(props) {
                             <div className="body">
                                 <p>
                                 <b>Escrow Account</b><br/>
-                                For now,. your loan<br/>
-                                <input type="checkbox"/> will have an esaow account (also called an•impound"or"trust"<br/>
-                                &nbsp; account) to pay the property costs listed below, Without an escrow<br/>
-                                &nbsp; account, you wwld Pit/ them directly, possibly in one or two large<br/>
-                                &nbsp; payments a year, Your lerder mitf be liable for penalties and <br/>
-                                &nbsp; interest for failing to make a payment.
+                                For now, your loan<br/>
+                                <input type="checkbox"/> your loan  will have an escrow account (also called an impound" or"trust" account) to pay the property costs listed below, 
+                                Without an escrow account, you will Pit/ them directly, possibly in one or two large payments a year, Your lender may be liable for penalties and interest
+                                for failing to make a payment. 
                                 </p>
                                 <>
                                 <Table >
@@ -123,17 +107,17 @@ function CDfourthpage(props) {
                                             <td>
                                                 <p>
                                                 Escrowed <br/>
-                                                Property Cos.ts. <br/>
+                                                Property Costs. <br/>
                                                 over Year 1 
                                                 </p>
                                             </td>
                                             <td>$8,054.00</td>
                                             <td>
                                                 <p>
-                                                Estimated total amountO\Efyear 1 for <br/>
-                                                your escrowed property cons: <br/>
-                                                Homeowner\ hu11ana <br/>
-                                                Ptcp,rtyT""" 
+                                                Estimated total amount over year 1 for
+                                                your escrowed property cons:
+                                                Homeowner Insurance
+                                                Property taxes
                                                 </p>
                                             </td>
                                         </tr>
@@ -148,11 +132,7 @@ function CDfourthpage(props) {
                                             <td>$ 1,900.00</td>
                                             <td>
                                                 <p>
-                                                Estimatedtotalamountcwryear 1 for <br/>
-                                                your non-escrtWed propel costs: <br/>
-                                                Homeowner\ Association ues <br/>
-                                                <br/>
-                                                You may ha\e other property costs..
+                                                Estimated total amount over year 1 for your non-escrowed Homeowner's  Association Dues  You may have other property costs.
                                                 </p>
                                             </td>
                                         </tr>
@@ -190,10 +170,10 @@ function CDfourthpage(props) {
                                 </Table>  
                                 </>
                                 <p>
-                                <input type="checkbox"/> will not have an escrow account because <input type="checkbox"/> you declined it <input type="checkbox"/> your
-                                &nbsp;lender does not offer one, 'lbu must directly pitf your prc:perty <br/>
-                                &nbsp;costs, such as taxes and homeowner"s insurance. Contact your <br/>
-                                &nbsp;lender to ask if your loan can have an escrow account.
+                                <input type="checkbox"/> will not have an escrow account because  
+                                 <input type="checkbox"/> you declined it  
+                                <input type="checkbox"/> your lender does not offer one, You must directly pay your property costs, such as taxes and homeowner"s insurance.
+                                 Contact your lender to ask if your loan can have an escrow account. 
                                 </p>
                                 <>
                                 <Table >
@@ -234,15 +214,12 @@ function CDfourthpage(props) {
                                 </>
                                 <p>
                                 <b>In the future</b> <br/>
-                                Ywr property costs may change and, as a result, your escrow payment mitf change. You may be able to cancel your escrow account, <br/>
-                                but if you do, you must Pit/ your property costs directly, If you fail <br/>
-                                to Pit/ your property taxes, your state or local govemm ent mitf ( 1) <br/>
-                                impose fines and penalties or (2) place a tax lien on th is property, If <br/>
-                                you fai I to pay a "'I of your property costs, your lender m it/ (1 ) add <br/>
-                                the amounts to your loan balance, (2) add an esaow account to your <br/>
-                                loan, or (3) require you to pay for property insu ranee that the I ender <br/>
-                                buys on your behal( which Ii kely would cost more and pro\/i de fewer <br/>
-                                benefits than what you cou Id buy on your own,
+                                Your property costs may change and, as a result, your escrow payment may change.
+                                 You may be able to cancel your escrow account, but if you do, you must Pit/ your property costs directly, 
+                                 If you fail to pay your property taxes, your state or local government may( 1) impose fines and penalties or (2) place a tax lien on this property, 
+                                 If you fail to pay any of your property costs, your lender may/ (1 ) add the amounts to your loan balance, 
+                                 (2) add an escrow account to your loan, or (3) require you to pay for property insurance that the lender buys on your behalf
+                                 (which Likely would cost more and provide fewer benefits than what you could buy on your own, 
                                 </p>        
                             </div>
                         </div>
