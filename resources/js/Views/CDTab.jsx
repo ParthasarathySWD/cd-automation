a8abe9f7c8e0dc73806f4e29fff0e3d55aa8a54d
shoban0102 <shoban.baarathi@avanzegroup.com>
2021-03-03 20:46:24 +0530

    CDTab activeFix

--- resources/js/Views/CDTab.jsx
+++ resources/js/Views/CDTab.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './CDDetails.css';
 function CDTab() {
     return (
         <>
            <div className="col-lg-12 col-md-12 col-sm-12">
                 <ul className="nav nav-tabs3">
-                    <li className="nav-item"><Link to="/CDdetails" className="nav-link"  >Page 1</Link></li>
+                    <li className="nav-item"><NavLink to="/CDdetails" className="nav-link"  >Page 1</NavLink></li>
                     
-                    <li className="nav-item" ><Link to="/CDsecondpage" className="nav-link">Page 2</Link></li>
-                    <li className="nav-item"><Link to="/CDthirdpage" className="nav-link" >Page 3</Link></li>
-                    <li className="nav-item"><Link to="/CDfourthpage" className="nav-link" >Page 4</Link></li>
-                    <li className="nav-item"><Link to="/CDfifthpage" className="nav-link" >Page 5</Link></li>
+                    <li className="nav-item" ><NavLink to="/CDsecondpage" className="nav-link">Page 2</NavLink></li>
+                    <li className="nav-item"><NavLink to="/CDthirdpage" className="nav-link" >Page 3</NavLink></li>
+                    <li className="nav-item"><NavLink to="/CDfourthpage" className="nav-link" >Page 4</NavLink></li>
+                    <li className="nav-item"><NavLink to="/CDfifthpage" className="nav-link" >Page 5</NavLink></li>
                 </ul>
             </div> 
         </>
