bfd9e96c98957be53e8e67c17c74b20268cab9c6
kavin.kumar <kavin.kumar@avanzegroup.com>
2021-03-10 11:17:02 +0530

    CDDetails 1page

--- resources/views/CDDocument.blade.php
+++ resources/views/CDDocument.blade.php
@@ -3,30 +3,117 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        .col-6{
-            width:50%;
-
-        }
-        .row{
-            width:100%;
-            marign:-5px;
-        }
+      .second
+      {
+          position:absolute;
+          left:300px;
+      }
+      td{
+          font-size=12px;
+      }
+      .secondalign{
+        position:absolute;
+          left:600px;
+      }
     </style>
 </head>
 <body>
-<div class="row">
-    <div class="col-6">
-        <h3>Closing Disclosure</h3>
-    </div>
-
-    <div class="col-6">
-        <p>This form is a statement of final loan terms and closing casts. Compare this
-document with your Loan Estimate. </p>
-    </div>
-    </div>
-
-</body>
+<h5>Closing Disclosure </h5>
+<div class="second">
+            <h6>This form is a statement of final loan terms and closing costs. Compare this
+                document with your loan Estimate. </h6>
+        </div>
+           <h5>Closing Information</h5>
+                    <table>
+                     <tbody>
+                         <tr>
+                             <td><label>Date Issued </label></td>
+                             <td><input type="date"/></td>
+                         </tr>
+                         <tr>
+                             <td><label>Closing Date </label></td>
+                             <td><input type="date"/></td>
+                         </tr>
+                         <tr>
+                             <td><label>Disbursement Date </label></td>
+                             <td><input type="date"/></td>
+                         </tr>
+                         <tr>
+                            <td><label>Settlement Agent  </label></td>
+                        </tr>
+                        <tr>
+                            <td><label>File#  </label></td>
+                            <td>T206369</td>
+                        </tr>
+                        <tr>
+                            <td><label>Property  </label></td>
+                            <td>6823 Rene St <br/>Shawnee, KS 66216</td>
+                        </tr>
+                        <tr>
+                            <td><label>Estimated Prop. Value </label> </td>
+                           <td> 200,000</td>
+                        </tr>
+                    </tbody>
+                </table>    
+           <h5>Transaction Information</h5>
+           <table style="table-layout: fixed;border-collapse: collapse;width: 100%; margin-top: 34pt;">
+<tr>
+                <th class="pos" style="width: 2cm">Loan Terms</th>
+                <th class="bez" style="width: 4cm"></th>
+                <th class="mwst" style="width: 3.25cm"></th>
+                <th class="ein" style="width: 4cm">Can this amount increase after closing?</th>
+               
+</tr>
+<tr>
+<th class="pos" style="width: 2cm">Loan Amount</th>
+<td class="pos" style="width: 2cm">$182,998</td>
+<td class="pos" style="width: 4cm"></td>
+<td class="pos" style="width: 2cm">NO</td>
+</tr>
+<tr>
+<th class="pos" style="width: 2cm">Interest Rate</th>
+<td class="pos" style="width: 2cm">2.875% </td>
+<td class="pos" style="width: 4cm"></td>
+<td class="pos" style="width: 2cm">NO</td>
+</tr>
+<tr>
+<th class="pos" style="width: 2cm">Monthly Principal & Interest</th>
+<td class="pos" style="width: 2cm">$806.20 </td>
+<td class="pos" style="width: 4cm"></td>
+<td class="pos" style="width: 2cm">NO</td>
+</tr>
+<tr>
+<th class="pos" style="width: 2cm">Prepayment Penalty</th>
+<td class="pos" style="width: 2cm"></td>
+<td class="pos" style="width: 4cm"></td>
+<td class="pos" style="width: 2cm">NO</td>
+</tr>
+<tr>
+<th class="pos" style="width: 2cm">Balloon Payment</th>
+<td class="pos" style="width: 2cm"></td>
+<td class="pos" style="width: 4cm"></td>
+<td class="pos" style="width: 2cm">NO</td>
+</tr>
+</table>
+<h5>Projected Payments </h5>
+           <table style="table-layout: fixed;border-collapse: collapse;width: 100%; margin-top: 34pt;">
+            <tr>
+                <th class="pos" style="width: 5cm">Payment Calculation</th>
+                <th class="bez" style="width: 5cm">Years 1-11 </th>
+                <!-- <th class="mwst" style="width: 3.25cm"></th> -->
+                <th class="ein" style="width: 5cm">Years 12-28</th>
+            </tr>
+            <tr>
+            <td class="pos" style="width: 5cm">Principal & Interest </td>
+            <td class="pos" style="width: 5cm">$806.20</td>
+            <td class="pos" style="width: 5cm">$806.20</td>
+            </tr>
+    </table>
+ </body>
 </html>
