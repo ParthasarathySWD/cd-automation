7cfe9a384bb95b5afcf689fc2af6b8a00d592f5e
Parthasarathy <parthaintodesk@gmail.com>
2021-03-15 21:57:51 +0530

    Loader Added

---
+++ resources/js/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loader(props) {
+    return (
+        <div class="cell_click_loader hidden">
+            <i data-loading-icon="oval" class="w-5 h-5"></i>
+        </div>
+    );
+}
+
+export default Loader;
