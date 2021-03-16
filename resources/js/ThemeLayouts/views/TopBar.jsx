f306d8f78d86ed78e87af743960efb36c691f78b
boobalan786 <boobalan.k@avanzegroup.com>
2021-03-16 14:40:41 +0530

    Notes UI

--- resources/js/ThemeLayouts/views/TopBar.jsx
+++ resources/js/ThemeLayouts/views/TopBar.jsx
@@ -14,7 +14,7 @@ function TopBar(props) {
             <div className="h-full flex items-center">
                 
                 <a href="" className="-intro-x hidden md:flex">
-                    <img alt="Rubick Tailwind HTML Admin Template" className="w-6" src="images/logo.svg" />
+                    <img alt="Rubick Tailwind HTML Admin Template" className="w-6" src="../images/logo.svg" />
                     <span className="text-white text-lg ml-3"> Get<span className="font-medium">CDNow</span> </span>
                 </a>
                 
